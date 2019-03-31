a063419225c8f67f77644bd791fc30bc600905ba
IssamTechs <abdelnabi.issam@outlook.com>
2019-03-31 10:31:59 -0400

    update-test

--- ProductApp.Tests/UnitTest1.cs
+++ ProductApp.Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 using ProductApp.Controllers;
+using ProductApp.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace ProductApp.Tests
@@ -10,7 +13,9 @@ namespace ProductApp.Tests
         public void Test1()
         {
             var controller = new EmployeesController(new Services.EmployeeService());
-            var result = controller.Get();
+            var result = controller.Get() as IEnumerable<Employee>;
+            Assert.NotNull(result);
+            Assert.Equal(51, result.Count());
         }
     }
 }
